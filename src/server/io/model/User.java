c71d0cd6927d49d42d43cea41dbd0641e56d843b
Seena Rowhani <seenarowhani95@gmail.com>
2017-10-02 18:08:33 -0400

    test(refactor): Implement getters/setters of UserModel

--- src/server/io/model/User.java
+++ src/server/io/model/User.java
@@ -1,13 +1,42 @@
 package server.io.model;
 
 public class User {
-	int id;
-	String username;
-	String password;
+	private int id;
+	private String username;
+	private String password;
 	
 	public User(int userId, String username, String password) {
 		this.id = id;
 		this.password = password;
 		this.username = username;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	@Override
+	public String toString(){
+		return String.format("[%s, %s, %s]", id, username, password);
 	}
 }
