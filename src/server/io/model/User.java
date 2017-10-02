14890df8116d71ba0d124493df3d6a985d413b5c
Seena Rowhani <seenarowhani95@gmail.com>
2017-10-02 18:09:48 -0400

    fix(constructor): Fix constructor params

--- src/server/io/model/User.java
+++ src/server/io/model/User.java
@@ -5,7 +5,7 @@ public class User {
 	private String username;
 	private String password;
 	
-	public User(int userId, String username, String password) {
+	public User(int id, String username, String password) {
 		this.id = id;
 		this.password = password;
 		this.username = username;
