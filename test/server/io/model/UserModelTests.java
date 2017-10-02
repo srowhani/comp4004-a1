19653016b0aa0f08d302271e45bc75962f59f8b7
Seena Rowhani <seenarowhani95@gmail.com>
2017-10-02 18:06:20 -0400

    test(tdd): Add failing tests to UserModel to assert getters/setters are available

--- test/server/io/model/UserModelTests.java
+++ test/server/io/model/UserModelTests.java
@@ -2,12 +2,26 @@ package server.io.model;
 
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
 public class UserModelTests {
-    
+
     @Test
     public void canInstantiate () {
         assertNotNull(new User(1, "username", "password"));
+    }
+
+    @Test
+    public void canAccessProperties () {
+        int userId = 1;
+        String username = "testuser";
+        String password = "password";
+        User testUser = new User(userId, username, password);
+
+        assertEquals(testUser.getId(), userId);
+        assertEquals(testUser.getUsername(), username);
+        assertEquals(testUser.getPassword(), password);
+
     }
 }
