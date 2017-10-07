64fe1b7d57bfa06650ed93ba3d8790334900cea3
Seena Rowhani <seenarowhani95@gmail.com>
2017-10-07 13:54:44 -0400

    test(tdd): ClientTest

---
+++ test/main/java/server/io/handler/ClientTests.java
@@ -0,0 +1,25 @@
+package main.java.server.io.handler;
+
+import main.java.server.io.handler.model.Client;
+import main.java.server.io.handler.model.ClientState;
+import org.junit.Test;
+
+import static lib.Assert.assertDoesNotThrow;
+import static org.junit.Assert.assertEquals;
+
+public class ClientTests {
+    @Test
+    public void ableToInstantiate () {
+        assertDoesNotThrow(() -> new Client());
+    }
+
+    @Test
+    public void setClientState () {
+        Client c = new Client();
+        c.setState(ClientState.ONLINE);
+        assertEquals(c.getState(), ClientState.ONLINE);
+        c.setState(null);
+        assertEquals(c.getState(), null);
+
+    }
+}
