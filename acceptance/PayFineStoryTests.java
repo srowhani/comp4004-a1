baaf8c53868055e40703baea0524f11a0332b7e0
Seena Rowhani <seenarowhani95@gmail.com>
2017-10-09 21:39:38 -0400

    more stuff

---
+++ acceptance/PayFineStoryTests.java
@@ -0,0 +1,23 @@
+import org.junit.Test;
+
+import static org.junit.Assert.fail;
+
+public class PayFineStoryTests extends AcceptanceTest {
+    // If user privilege is revoked, the user’s privileges is returned.
+    @Test
+    public void userPrivilege () {
+        fail();
+        // create user
+        // create loan
+        // pay loan on later date
+        // try to withdraw, and fail
+    }
+
+    @Test
+    public void userAllowedToReturnItemsAfterPayingFine () {
+        fail();
+    }
+
+    // User must be allowed to borrow books again.
+
+}
